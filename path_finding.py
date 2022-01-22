8fee237dc70a7c5c711e791da96b099ce4bb1681
d3tk <dkutscher10@gmail.com>
2022-01-22 13:19:31 -0500

    commit to main

--- path_finding.py
+++ path_finding.py
@@ -1,5 +1,3 @@
-from tkinter import messagebox
-
 
 try:
     import pygame
@@ -13,12 +11,59 @@ try:
 except:
     import install_requirements
 
-    import pygame
+    import pygame 
     import sys
     import math
     from tkinter import *
     from tkinter import ttk
     from tkinter import messagebox
     import os
+
+screen = pygame.display.set_mode((800,800))
+
+class spot:
+    def __init__(self, x, y):
+        self.i = X
+        self.j = y
+        self.f = 0
+        self.g = 0
+        self.h = 0
+        self.neighbors = []
+        self.previous = None 
+        self.obs = False 
+        self.closed = False 
+        self.value = 1 
+
+    def show(self, color, st):
+        if self.closed == False:
+            pygame.draw.rect(screen, color, (self.i*w, self.j*h, w, h), st)
+            pygame.display.update()
+    
+    def path(self, color, st): 
+        pygame.draw.rect(screen, color, (self.i*W, self.j*h, w, h), st) 
+        pygame.display.update() 
+
+cols = 50
+grid = (0 for i in range(cols))
+rows = 50
+openset = []
+closedset = []
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+grey = (220, 220, 220)
+w = 800/cols 
+h = 800/rows 
+camefrom = [] 
+
+for i in range(cols): 
+    grid[i] = [0 for i in range(cols)]
+
+for i in range(cols):
+    for j in range(rows):
+        grid[i][j].show((255,255,255))
+
+
+    
 
 
