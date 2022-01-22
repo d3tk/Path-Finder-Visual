1bf3710334d46a72923f5ce04acf73b92b1657f0
d3tk <dkutscher10@gmail.com>
2022-01-22 12:35:46 -0500

    commit to main

--- path_finding.py
+++ path_finding.py
@@ -1,1 +1,24 @@
+from tkinter import messagebox
+
+
+try:
+    import pygame
+    import sys
+    import math 
+    from tkinter import *
+    from tkinter import ttk
+    from tkinter import messagebox
+    import os
+
+except:
+    import install_requirements
+
+    import pygame
+    import sys
+    import math
+    from tkinter import *
+    from tkinter import ttk
+    from tkinter import messagebox
+    import os
+
 
